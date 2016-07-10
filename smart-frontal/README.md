a602689f47c442f634396a697579c061778a3e2e
yunxiaoxie <weistar103@163.com>
2016-07-10 19:46:40 +0800

    create smart-frontal project

---
+++ smart-frontal/README.md
@@ -0,0 +1,9 @@
+# Smart Frontal
+
+## 前端工程简介
+
+- 前端工程采用gulp/npm管理项目
+
+## 安装工程依赖
+- bower install --install js dependents to local lib dir.
+- npm install --install dependents to local node_modules dir.
---
+++ smart-frontal/gulpfile.js
@@ -0,0 +1,96 @@
+'use strict';
+// 载入外挂
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    cssimport = require("gulp-cssimport"),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    less = require('gulp-less');
+
+
+gulp.task('bootstrap', function() {
+    return gulp.src('public/javascripts/lib/bootstrap/less/bootstrap.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/javascripts/lib/bootstrap/dist/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss({
+            processImport: true
+        }))
+        .pipe(gulp.dest('public/javascripts/lib/bootstrap/dist/css'))
+        .pipe(notify({ message: 'bootstrap task complete' }));
+});
+// 样式
+gulp.task('styles', function() {
+    return sass('css/main.scss', { style: 'expanded'})
+        .pipe(cssimport())
+        .pipe(concat('main.css'))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss({
+            processImport: true
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// 脚本
+gulp.task('scripts', function() {
+    return gulp.src('js/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// 图片
+gulp.task('images', function() {
+    return gulp.src('public/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('public/dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+// 清理
+gulp.task('clean', function() {
+    return gulp.src(['public/stylesheets', 'public/javascripts', 'public/images'], {read: false})
+        .pipe(clean());
+});
+
+// 预设任务
+gulp.task('default',['styles', 'scripts', 'images']);
+
+// 看手
+gulp.task('watch', function() {
+
+    // 看守所有.scss档
+    gulp.watch('public/stylesheets/**/*.scss', ['styles']);
+
+    // 看守所有.js档
+    gulp.watch('public/javascripts/**/*.js', ['scripts']);
+
+    // 看守所有图片档
+    gulp.watch('public/images/**/*', ['images']);
+
+    // 建立即时重整伺服器
+    var server = livereload();
+
+    // 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
+    gulp.watch(['public/dist/**']).on('change', function(file) {
+        server.changed(file.path);
+    });
+
+});
